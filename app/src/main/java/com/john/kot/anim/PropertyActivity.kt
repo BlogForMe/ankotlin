68273b264323e6c8b90fb400c750bf8231f5505a
john <huaiyiheyuan@gmail.com>
2019-12-25 17:26:52 +0800

    血压绘制

---
+++ app/src/main/java/com/john/kot/anim/PropertyActivity.kt
@@ -0,0 +1,12 @@
+package com.john.kot.anim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PropertyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_property)
+    }
+}
--- app/src/main/java/com/john/kot/view/PressResultView.kt
+++ app/src/main/java/com/john/kot/view/PressResultView.kt
@@ -7,6 +7,8 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import com.john.kot.R
+import kotlinx.android.synthetic.main.activity_skin_theme.view.*
 
 /**
  * 绘制血压结果图
@@ -15,23 +17,47 @@ class PressResultView @JvmOverloads constructor(context: Context?, attrs: Attrib
                                                 defStyleAttr: Int=0):View(context, attrs, defStyleAttr){
 
 
+    private var rectHight: Float
+    private var lineHeight: Float
     val mRtSrcPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val mLinePaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
 
     init {
+        rectHight = 100f
+        lineHeight = 40f;
+
         mRtSrcPaint.apply {
             color = Color.parseColor("#A2CB79")
             strokeWidth = 1f
             style = Paint.Style.STROKE
             strokeCap = Paint.Cap.ROUND
-            textSize = 28f
+        }
+
+        mLinePaint.apply {
+            color = resources.getColor(android.R.color.holo_blue_light,context?.theme)
+            strokeWidth = lineHeight
+            style = Paint.Style.STROKE
+            strokeCap = Paint.Cap.ROUND
         }
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        val rectF = RectF(0f,100f,1000f,50f)
-        canvas?.drawRoundRect(rectF,100f,500f,mRtSrcPaint)
+
+        val centerX = width/2f
+        val centerY = height/2f
+
+        var lineY = rectHight / 2
+
+        canvas?.drawLine(0f,lineY,400f,lineY,mLinePaint)
+
+        val rectF = RectF(0f,0f,1000f,rectHight)
+
+        canvas?.drawRoundRect(rectF,centerX,centerY,mRtSrcPaint)
+
+
+
     }
 
 }
