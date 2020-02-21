e1c14f8ce5bcb754f2125a511ad4c47374df2d33
john <huaiyiheyuan@gmail.com>
2020-02-21 22:49:07 +0800

    去除背景温度计

--- app/src/main/java/com/john/kot/ui/CircleProgressActivity.kt
+++ app/src/main/java/com/john/kot/ui/CircleProgressActivity.kt
@@ -1,8 +1,11 @@
 package com.john.kot.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.john.kot.R
+import kotlinx.android.synthetic.main.ble_activity_temp.*
+import timber.log.Timber
 
 class CircleProgressActivity : AppCompatActivity() {
 
@@ -10,5 +13,10 @@ class CircleProgressActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ble_activity_temp)
 //        setContentView(R.layout.activity_circle_progress)
+
+
+//        tv_confirm.setOnClickListener {
+//            Timber.i("degree    "+trm_rule.getDegree())
+//        }
     }
 }
--- app/src/main/java/com/john/kot/view/TermRoundView.kt
+++ app/src/main/java/com/john/kot/view/TermRoundView.kt
@@ -3,37 +3,50 @@ package com.john.kot.view
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.view.View
-import com.john.kot.R
-import java.lang.ref.WeakReference
-import android.graphics.BitmapFactory
-import android.graphics.Bitmap
 import android.view.MotionEvent
+import android.view.View
+import com.comm.util.DisplayUtils.dp2px
 import timber.log.Timber
 import java.text.DecimalFormat
 
-
+/**
+ *  a Thermometer
+ */
 class TermRoundView @JvmOverloads constructor(
     context: Context?, attrs: AttributeSet?,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+    private var degreeDf: String? = null
     private var scaleUnit: Float = 0.0f
-    private val srcPaint: Paint
-    private val innnerPaint: Paint
-    private val innerRadius = 35f
+    private val srcPaint: Paint //外圆环
+    private val innnerPaint: Paint //内圆
+    private val innerRadius = dp2px(context,20f) //内圆半径
 
-    private val txtTitlePaint: Paint
     private val linePaint: Paint
     private val txtPaint: Paint
     private val yellowColorPaint: Paint
     private val yellowLightPaint: Paint
-    val startX = 22f+166
-    var scrollX = startX + 875/8 *2
+    private val yellowRectPaint: Paint
+    private val metalPaint: Paint   //黄色矩形线
+    val rulerLength=dp2px(context,730f) //温度计到头部到刻度42总长度
+    val startX = dp2px(context,126f) //左边距离
+    var tempScale =  (rulerLength- startX)/ (2 * 8) //刻度线长度
+    val metalWidth= dp2px(context,40f) //the metal of Thermometer
+
+    var scrollX:Float
+
+    val rulerY = dp2px(context,200f)
+    val waterHeight  = 15 //水银高度
+    val longHScale = 20 //长刻度线的长度
+
+    val txtTop =60f
+
 
     init {
         isClickable = true
-        srcPaint = Paint().apply {
+        scrollX = (startX + tempScale *2 *2) //默认36
+        srcPaint = Paint().apply {  //外圆
             color = Color.parseColor("#37BC95");
         }
 
@@ -41,16 +54,13 @@ class TermRoundView @JvmOverloads constructor(
             color = Color.parseColor("#A2CB79");
         }
 
-
-
-        txtTitlePaint = Paint().apply {
-            color = Color.parseColor("#EF8741");
-            textSize = 37f
-        }
-
         linePaint = Paint().apply {
             color = Color.parseColor("#999999");
             style = Paint.Style.STROKE
+        }
+
+        metalPaint = Paint().apply {
+            color = Color.parseColor("#c0c0c2");
         }
 
         txtPaint = Paint().apply {
@@ -67,50 +77,37 @@ class TermRoundView @JvmOverloads constructor(
             color = Color.parseColor("#FCF0D1");
         }
 
+        yellowRectPaint = Paint().apply {
+            color = Color.parseColor("#EF8741");
+        }
 
+        scaleUnit = tempScale /5f; //0.1对应的长度
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        val txtTop =60f
-
-        val bitmapWeak = WeakReference<Bitmap>(
-                    BitmapFactory.decodeResource(
-                context.resources,
-                R.mipmap.bg_temprature
-            )
-        )
-
-        val ruleTop = 100f
-        var rulerBitmap = bitmapWeak.get()
-        canvas?.drawBitmap(rulerBitmap,0f,ruleTop,Paint())
-//        Timber.i("bitmap width ${rulerBitmap?.width}  height ${rulerBitmap?.height}")
-        val imgHeight = rulerBitmap?.height!!.toFloat()
-//       val medleRect = RectF(0f,0f,rulerBitmap.width.toFloat(),20f);
         //中线
-        val waterHeight  = 15
-        val topF =(ruleTop+imgHeight/2)-waterHeight/2;
-        val bottomF = waterHeight/2+(ruleTop+imgHeight/2);
-        val middleRect =  RectF(45f,topF,rulerBitmap?.width.toFloat()-10,bottomF)
+        val topF = rulerY-waterHeight/2
+        val bottomF = rulerY+waterHeight/2;
+        val metalRect =  RectF(0f,topF-3,metalWidth,bottomF+3)
+        canvas?.drawRoundRect(metalRect,10f,10f,metalPaint)
+
+        val middleRect =  RectF(metalWidth,topF, dp2px(context,728f),bottomF)
         canvas?.drawRoundRect(middleRect,10f,10f,linePaint)
 
-        val yellowRect =  RectF(45f,topF,scrollX,bottomF)
-        canvas?.drawRect(yellowRect,yellowColorPaint)
+//        Timber.i("topF  $topF bottomF   $bottomF    rulerLength $rulerLength")
 
-        Timber.i("rulerBitmap?.width    ${rulerBitmap?.width}")
-//        var tempScale = (rulerBitmap?.width.toFloat() - 30f - 42f) / (2 * 9) //刻度线长度
-        var tempScale = 870f / (2 * 8) //刻度线长度
+        val yellowRect =  RectF(metalWidth,topF,scrollX,bottomF)
+        canvas?.drawRect(yellowRect,yellowRectPaint)
 
-        scaleUnit = tempScale /5; //0.1对应的长度
 
-        val longHScale = 20
 
         //刻度
         for(i in 0..16){
             if ((i+1)%2==0){
-                canvas?.drawLine(startX+tempScale*i,topF-5,startX+tempScale*i,topF,linePaint)
-                canvas?.drawLine(startX+tempScale*i,bottomF,startX+tempScale*i,bottomF+5,linePaint)
+                canvas?.drawLine(startX+tempScale*i,rulerY-waterHeight/2-5,startX+tempScale*i,rulerY-waterHeight/2,linePaint)
+                canvas?.drawLine(startX+tempScale*i,rulerY+waterHeight/2,startX+tempScale*i,rulerY+waterHeight/2+5,linePaint)
             }
 
             val drawTxt = (34+i/2).toString()
@@ -118,48 +115,47 @@ class TermRoundView @JvmOverloads constructor(
             txtPaint.getTextBounds(drawTxt,0,drawTxt.length,txtRect)
             //底部长线
             if (i%4==0){
-                canvas?.drawLine(startX+tempScale*i,bottomF,startX+tempScale*i,bottomF+longHScale,linePaint)
-
-                canvas?.drawText(drawTxt,startX+tempScale*i - txtRect.width()/2,bottomF+longHScale+txtRect.height(),txtPaint)
+                canvas?.drawLine(startX+tempScale*i,rulerY+waterHeight/2,startX+tempScale*i,rulerY+waterHeight/2+longHScale,linePaint)
+                canvas?.drawText(drawTxt,startX+tempScale*i - txtRect.width()/2,rulerY+waterHeight/2+longHScale+txtRect.height(),txtPaint)
             }
 
             //头部长线
             if ((i+2)%4==0){
-                canvas?.drawLine(startX+tempScale*i,topF-longHScale,startX+tempScale*i,topF,linePaint)
-                canvas?.drawText(drawTxt,startX+tempScale*i-txtRect.width()/2,topF-longHScale,txtPaint)
+                canvas?.drawLine(startX+tempScale*i,rulerY-waterHeight/2-longHScale,startX+tempScale*i,rulerY-waterHeight/2,linePaint)
+                canvas?.drawText(drawTxt,startX+tempScale*i-txtRect.width()/2,rulerY-waterHeight/2-longHScale,txtPaint)
             }
         }
-
-        val centerY = (topF + longHScale/2)
         val srcRadius: Float = (innerRadius + 8)
-//        Timber.i("scrollX   $scrollX   centerY  $centerY")
-        canvas?.drawCircle(scrollX, centerY, srcRadius, srcPaint);
-        canvas?.drawCircle(scrollX, centerY, innerRadius, innnerPaint);
+        canvas?.drawCircle(scrollX, rulerY, srcRadius, srcPaint);
+        canvas?.drawCircle(scrollX, rulerY, innerRadius, innnerPaint);
 
-        var rectCircleFirstX = (scrollX-innerRadius + 10) //第一个圆柱坐标
+        var rectCircleFirstX = (scrollX- dp2px(context,5f)/2-10 - dp2px(context,5f)) //第一个圆柱坐标,10是圆柱间距
         for (i in 0..2) {
-            val rectF = RectF(rectCircleFirstX+(10*i), centerY-20f, rectCircleFirstX+10f+(10*i), centerY+20);
+            val rectF = RectF(rectCircleFirstX+(10*i), rulerY-20f, rectCircleFirstX+10f+(10*i), rulerY+20);
             rectCircleFirstX = (rectCircleFirstX + 10);
             canvas?.drawRoundRect(rectF, 10f, 10f, srcPaint)
         }
-        Timber.i("onDraw scrollX   $scrollX")
+//        Timber.i("onDraw scrollX   $scrollX")
+        val df = DecimalFormat("#0.0")
+        val degreeValue =(scrollX - startX) / (scaleUnit*10) + 34
+        degreeDf=df.format(degreeValue)
 
-        val df =DecimalFormat("#0.0")
-        val degree=df.format((scrollX - startX) / (scaleUnit*10) + 34)
-
-
-        val showDegreeTxt =degree.plus("℃")
+        val showDegreeTxt =degreeDf.plus("℃")
 
         val bgRect = Rect()
         yellowColorPaint.getTextBounds(showDegreeTxt,0,showDegreeTxt.length,bgRect)
 
-        val rf = RectF(300f-20,txtTop-20-bgRect.height(),300f+bgRect.width()+20,txtTop+20)
-        canvas?.drawRect(rf,yellowLightPaint)
-
-        Timber.i(" onDraw degree  $degree   startX  $startX")
-        canvas?.drawText(showDegreeTxt, 300f, txtTop, yellowColorPaint)
-
-
+        val rf = RectF(width/2f-20,txtTop-20-bgRect.height(),width/2f+bgRect.width()+20,txtTop+20)
+        canvas?.drawRect(rf,yellowLightPaint) //度数边框
+//        Timber.i(" onDraw degree  $   startX  $startX")
+        if (degreeValue<37.5){
+            yellowColorPaint.color = Color.parseColor("#A2CB79")
+        }else if(degreeValue>=37.5 && degreeValue<=38){
+            yellowColorPaint.color = Color.parseColor("#EF8741")
+        }else if (degreeValue>38){
+            yellowColorPaint.color = Color.parseColor("#EC4736")
+        }
+        canvas?.drawText(showDegreeTxt, width/2f, txtTop, yellowColorPaint) //度数显示
     }
 
 
@@ -167,7 +163,7 @@ class TermRoundView @JvmOverloads constructor(
 //    var anOffset :Float = 0.toFloat()
     override fun onTouchEvent(event: MotionEvent): Boolean {
         Timber.i("action onTouchEvent: " + event.action)
-
+        if (event.x>=startX && event.x <= (rulerLength))
         when(event?.action){
             MotionEvent.ACTION_DOWN-> {
                 downX = event.x
@@ -175,21 +171,21 @@ class TermRoundView @JvmOverloads constructor(
             }
 
             MotionEvent.ACTION_MOVE -> {
-//                anOffset = scrollX - downX
 //                //向右滑动
-//                Timber.i("onTouchEvent ACTION_MOVE    scrollX   $scrollX    downX   $downX")
-
                 Timber.i("onTouchEvent ACTION_MOVE    Math.abs(scrollX-downX) ${Math.abs(scrollX-downX)}  scaleUnit  $scaleUnit ")
                 scrollX = event.x
-                if (Math.abs(scrollX-downX)>=scaleUnit){
+                if (Math.abs(scrollX-downX)>=(scaleUnit/3)){
                     Timber.i("onTouchEvent ACTION_MOVE    invalidate()")
                     invalidate()
                 }
-
             }
         }
-
         return super.onTouchEvent(event)
+    }
+
+
+    fun  getDegree():String?{
+        return degreeDf
     }
 
 }
