62bbf3eb65d90e44265ebbcf4e26f5787010ce87
zhouheng <zhouheng@tngdigital.com>
2022-10-15 18:55:18 +0800

    feat: Hilt update

--- app/src/main/java/com/john/kot/hilt/HiltActivity.kt
+++ app/src/main/java/com/john/kot/hilt/HiltActivity.kt
@@ -1,24 +1,22 @@
-package com.john.kot.hilt;
+package com.john.kot.hilt
 
-import javax.inject.Inject;
-
-import android.os.Bundle;
-import android.util.Log;
-import androidx.appcompat.app.AppCompatActivity;
-import com.john.kot.R;
-import dagger.hilt.android.AndroidEntryPoint;
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.john.kot.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-public class HiltActivity extends AppCompatActivity {
-
-    String TAG = "HiltActivity";
+class HiltActivity : AppCompatActivity() {
+    var TAG = "HiltActivity"
 
     @Inject
-    User1 user1; // 方式2
+    lateinit var user1: User1 // 方式2
+
 
     //@Inject
     //User1 user2;
-
     //@Inject
     //ViewModel viewModel;
     //
@@ -27,13 +25,11 @@ public class HiltActivity extends AppCompatActivity {
     //
     //@Inject
     //ViewModel3 viewModel3;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hilt);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hilt)
         //测试  ActivityComponent
-        Log.i(TAG, "user1: " + user1);
+        Log.i(TAG, "user1: $user1")
         //Log.i(TAG, "user2: " + user2);
         //startActivity(new Intent(this, SecondActivity.class));
 
@@ -45,6 +41,5 @@ public class HiltActivity extends AppCompatActivity {
         //viewModel.test();
         //
         //Log.i(TAG, "onCreate:  "+ getScreenW(this) +" " + getScreenH(this));
-
     }
 }
--- app/src/main/java/com/john/kot/mvvm/livedata/LiveDataTestActivity.kt
+++ app/src/main/java/com/john/kot/mvvm/livedata/LiveDataTestActivity.kt
@@ -1,39 +1,34 @@
 package com.john.kot.mvvm.livedata
 
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.john.kot.R
 
 /**
  * https://www.jianshu.com/p/e08287ec62cd
  */
 class LiveDataTestActivity : AppCompatActivity() {
-    private var mViewModel: CustomViewModel? = null
+//    private var mViewModel: CustomViewModel? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_live_data)
-        mViewModel = ViewModelProvider(this).get(CustomViewModel::class.java)
-        val liveData = mViewModel!!.liveData
-        liveData.observe(this, object : Observer<Int> {
-            override fun onChanged(integer: Int) {
-                Log.e(TAG, "参数返回： $integer")
-                liveData.removeObserver(this)
-            }
-        })
-    }
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_live_data)
+//        mViewModel = ViewModelProvider(this).get(CustomViewModel::class.java)
+//        val liveData = mViewModel!!.liveData
+//        liveData.observe(this, object : Observer<Int> {
+//            override fun onChanged(integer: Int) {
+//                Log.e(TAG, "参数返回： $integer")
+//                liveData.removeObserver(this)
+//            }
+//        })
+}
 
     override fun onResume() {
         super.onResume()
-        Handler().postDelayed(Runnable {
-            val liveData = mViewModel!!.liveData
-            liveData.observe(this
-            ) { integer -> Log.e(TAG, "参数返回： $integer") }
-        }, 5000)
+//        Handler().postDelayed(Runnable {
+//            val liveData = mViewModel!!.liveData
+//            liveData.observe(this
+//            ) { integer -> Log.e(TAG, "参数返回： $integer") }
+//        }, 5000)
     }
 
     companion object {
