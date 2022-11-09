80e5709d502eee270196b444c8355900a4c346b2
zhouheng <zhouheng@tngdigital.com>
2022-11-09 10:41:48 +0800

    feat: featureA function

---
+++ app/src/main/java/com/FeatureA.kt
@@ -0,0 +1,15 @@
+package com
+
+/**
+ *
+ * ClassName:      FeatureA
+ * Description:    Description
+ * Author:         zh
+ * CreateDate:     2022/11/9
+ * UpdateUser:     zh
+ * UpdateDate:     2022/11/9
+ * UpdateRemark:   Modify the description
+ */
+
+class FeatureA {
+}
