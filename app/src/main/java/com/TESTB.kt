568c04ba5b9c3e4c259227e10292046628c32105
zhouheng <zhouheng@tngdigital.com>
2022-11-09 10:47:17 +0800

    feat:commit B

---
+++ app/src/main/java/com/TESTB.kt
@@ -0,0 +1,15 @@
+package com
+
+/**
+ *
+ * ClassName:      TESTB
+ * Description:    Description
+ * Author:         zh
+ * CreateDate:     2022/11/9
+ * UpdateUser:     zh
+ * UpdateDate:     2022/11/9
+ * UpdateRemark:   Modify the description
+ */
+
+class TESTB {
+}
