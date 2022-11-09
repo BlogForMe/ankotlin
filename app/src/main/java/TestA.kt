25a70ceedca5d6f8ead057b93c91659888828d91
zhouheng <zhouheng@tngdigital.com>
2022-11-09 10:45:19 +0800

    feat:commit A

---
+++ app/src/main/java/TestA.kt
@@ -0,0 +1,13 @@
+/**
+ *
+ * ClassName:      TestA
+ * Description:    Description
+ * Author:         zh
+ * CreateDate:     2022/11/9
+ * UpdateUser:     zh
+ * UpdateDate:     2022/11/9
+ * UpdateRemark:   Modify the description
+ */
+
+class TestA {
+}
