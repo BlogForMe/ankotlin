2bfe0b02be8493532513211a5653dbe83f9356dc
zhouheng <zhouheng@tngdigital.com>
2023-08-13 00:06:09 +0800

    target version

---
+++ app/src/main/java/com/compose/ComposeActivity.kt
@@ -0,0 +1,16 @@
+package com.compose
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+
+class ComposeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("hello world")
+        }
+
+    }
+}
