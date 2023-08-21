8167c5532a7e8d80e33342fb8a84ad4e7efe5d05
zhouheng <zhouheng@tngdigital.com>
2023-08-21 16:55:45 +0800

    tokotlin

--- app/src/main/java/com/kot/tool/shake/ShakeActivity.kt
+++ app/src/main/java/com/kot/tool/shake/ShakeActivity.kt
@@ -1,149 +1,128 @@
-package com.kot.tool.shake;
+package com.kot.tool.shake
 
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
-import android.app.AlertDialog;
-import android.app.Service;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.drawable.ColorDrawable;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.media.MediaPlayer;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Vibrator;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.Window;
-import android.widget.ImageView;
-import androidx.appcompat.app.AppCompatActivity;
-import com.kot.R;
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.Handler
+import android.os.Vibrator
+import android.view.LayoutInflater
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.kot.R
 
 /**
  * https://juejin.cn/post/6844903743595479054
  * https://github.com/taixiang/shake
  */
-public class ShakeActivity extends AppCompatActivity {
-
+class ShakeActivity : AppCompatActivity() {
     /**
      * 传感器
      */
-    private SensorManager sensorManager;
-    private ShakeSensorListener shakeListener;
+    private var sensorManager: SensorManager? = null
+    private var shakeListener: ShakeSensorListener? = null
 
     /**
      * 判断一次摇一摇动作
      */
-    private boolean isShake = false;
+    private var isShake = false
+    private var imgHand: ImageView? = null
 
-    private ImageView imgHand;
     /**
      * 摇一摇动画
      */
-    private ObjectAnimator anim;
+    private val anim: ObjectAnimator? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shake)
+        imgHand = findViewById(R.id.imgHand)
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        shakeListener = ShakeSensorListener()
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shake);
-        imgHand = findViewById(R.id.imgHand);
-        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        shakeListener = new ShakeSensorListener();
-
-        anim = ObjectAnimator.ofFloat(imgHand, "rotation", 0f, 45f, -30f, 0f);
-        anim.setDuration(500);
-        anim.setRepeatCount(ValueAnimator.INFINITE);
+        anim?.duration = 500
+        anim?.repeatCount = ValueAnimator.INFINITE
     }
 
-    @Override
-    protected void onResume() {
+    override fun onResume() {
         //注册监听加速度传感器
-        sensorManager.registerListener(shakeListener,
-            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-            SensorManager.SENSOR_DELAY_FASTEST);
-        super.onResume();
+        sensorManager?.registerListener(
+            shakeListener,
+            sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_FASTEST
+        )
+        super.onResume()
     }
 
-    @Override
-    protected void onPause() {
+    override fun onPause() {
         /**
          * 资源释放
          */
-        sensorManager.unregisterListener(shakeListener);
-        super.onPause();
+        sensorManager?.unregisterListener(shakeListener)
+        super.onPause()
     }
 
-    private class ShakeSensorListener implements SensorEventListener {
-
-        @Override
-        public void onSensorChanged(SensorEvent event) {
+    private inner class ShakeSensorListener : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
             //避免一直摇
             if (isShake) {
-                return;
+                return
             }
             // 开始动画
-            anim.start();
-            float[] values = event.values;
+            anim?.start()
+            val values = event.values
             /*
              * x : x轴方向的重力加速度，向右为正
              * y : y轴方向的重力加速度，向前为正
              * z : z轴方向的重力加速度，向上为正
              */
-            float x = Math.abs(values[0]);
-            float y = Math.abs(values[1]);
-            float z = Math.abs(values[2]);
+            val x = Math.abs(values[0])
+            val y = Math.abs(values[1])
+            val z = Math.abs(values[2])
             //加速度超过19，摇一摇成功
             if (x > 19 || y > 19 || z > 19) {
-                isShake = true;
+                isShake = true
                 //播放声音
-                playSound(ShakeActivity.this);
+                playSound(this@ShakeActivity)
                 //震动，注意权限
-                vibrate(500);
+                vibrate(500)
                 //仿网络延迟操作，这里可以去请求服务器...
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        //弹框
-                        showDialog();
-                        //动画取消
-                        anim.cancel();
-                    }
-                }, 1000);
+                Handler().postDelayed({ //弹框
+                    showDialog()
+                    //动画取消
+                    anim?.cancel()
+                }, 1000)
             }
         }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+    private fun playSound(context: Context) {
+        val player = MediaPlayer.create(context, R.raw.shake_sound)
+        player.start()
+    }
+
+    private fun vibrate(milliseconds: Long) {
+        val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+        vibrator.vibrate(milliseconds)
+    }
+
+    private fun showDialog() {
+        val mAlertDialog = AlertDialog.Builder(this).show()
+        val view = LayoutInflater.from(this).inflate(R.layout.layout_dialog, null)
+        mAlertDialog.setContentView(view)
+        mAlertDialog.setOnCancelListener { //这里让弹框取消后，才可以执行下一次的摇一摇
+            isShake = false
+            mAlertDialog.cancel()
         }
+        val window = mAlertDialog.window
+        window!!.setBackgroundDrawable(ColorDrawable(0x00000000))
     }
-
-    private void playSound(Context context) {
-        MediaPlayer player = MediaPlayer.create(context, R.raw.shake_sound);
-        player.start();
-    }
-
-    private void vibrate(long milliseconds) {
-        Vibrator vibrator = (Vibrator)getSystemService(Service.VIBRATOR_SERVICE);
-        vibrator.vibrate(milliseconds);
-    }
-
-    private void showDialog() {
-        final AlertDialog mAlertDialog = new AlertDialog.Builder(this).show();
-        View view = LayoutInflater.from(this).inflate(R.layout.layout_dialog, null);
-        mAlertDialog.setContentView(view);
-        mAlertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-            @Override
-            public void onCancel(DialogInterface dialog) {
-                //这里让弹框取消后，才可以执行下一次的摇一摇
-                isShake = false;
-                mAlertDialog.cancel();
-            }
-        });
-        Window window = mAlertDialog.getWindow();
-        window.setBackgroundDrawable(new ColorDrawable(0x00000000));
-    }
-
 }
