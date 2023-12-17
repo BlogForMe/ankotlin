3bad3531a505f9a1641cbe19db34c7dbf8de6dfb
zhouheng <zhouheng@tngdigital.com>
2023-12-17 21:36:07 +0800

    commit picker

---
+++ app/src/main/java/com/kot/ui/image/RingCircleImageActivity.kt
@@ -0,0 +1,12 @@
+package com.kot.ui.image
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kot.R
+
+class RingCircleImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ring_circle_image)
+    }
+}
