f176bf3d8bf59c8a8a932b41f45baa7fe03441cf
zhouheng <zhouheng@tngdigital.com>
2024-04-02 07:51:40 +0800

    restrict emoj

--- app/src/main/java/com/kot/ui/KeyBordActivity.kt
+++ app/src/main/java/com/kot/ui/KeyBordActivity.kt
@@ -1,15 +1,30 @@
 package com.kot.ui
 
 import android.os.Bundle
+import android.text.InputFilter
 import androidx.appcompat.app.AppCompatActivity
-import com.kot.R
+import com.android.util.viewbind.viewBinding
+import com.kot.databinding.ActivityKeyBordBinding
+
 
 class KeyBordActivity : AppCompatActivity() {
 
-//    val binding by viewBinding(activity::inflate)
+    val binding by viewBinding(ActivityKeyBordBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_key_bord)
+
+        binding.etText.filters = arrayOf(EMOJI_FILTER)
+
     }
+}
+
+var EMOJI_FILTER = InputFilter { source, start, end, dest, dstart, dend ->
+    for (index in start until end) {
+        val type = Character.getType(source[index])
+        if (type == Character.SURROGATE.toInt()) {
+            return@InputFilter ""
+        }
+    }
+    null
 }
