db45bfc2ee67eb354fca02ef8430018145b0d69c
zhouheng <zhouheng@tngdigital.com>
2024-04-02 09:40:11 +0800

    edittext

--- app/src/main/java/com/kot/ui/KeyBordActivity.kt
+++ app/src/main/java/com/kot/ui/KeyBordActivity.kt
@@ -1,10 +1,11 @@
 package com.kot.ui
 
 import android.os.Bundle
+import android.text.InputFilter
 import androidx.appcompat.app.AppCompatActivity
 import com.android.util.viewbind.viewBinding
-import com.kot.R
 import com.kot.databinding.ActivityKeyBordBinding
+
 
 class KeyBordActivity : AppCompatActivity() {
 
@@ -12,17 +13,28 @@ class KeyBordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_key_bord)
 
         // demo 中默认 LimitEditText 只能输入字母数字和标点符号
-
+//        binding.letMain
 
         // 延时主要是更方便观察
-        window.decorView.postDelayed({
-            // 注意,获得焦点需要自己再处理下,其实很简单,如下:
-            binding.letMain.isFocusable = true
-            binding.letMain.isFocusableInTouchMode = true
-            binding.letMain.requestFocus()
-        }, 1000)
+//        window.decorView.postDelayed({
+//            // 注意,获得焦点需要自己再处理下,其实很简单,如下:
+//            binding.letMain.isFocusable = true
+//            binding.letMain.isFocusableInTouchMode = true
+//            binding.letMain.requestFocus()
+//        }, 1000)
+
+        binding.letMain.filters =
+            arrayOf(filter)
     }
+    private val blockCharacterSet = "~#^|$%&*!";
+
+
+    private val filter =
+        InputFilter { source, start, end, dest, dstart, dend ->
+            if (source != null && blockCharacterSet.contains("" + source)) {
+                ""
+            } else null
+        }
 }
